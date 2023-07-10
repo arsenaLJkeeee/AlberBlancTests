326b85588fd42dc5b41716ef915f0419f408c8e2
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2023-07-10 21:30:47 +0300

    edit testbase

--- src/test/java/com/albertblanc/AlberBlancTestBase.java
+++ src/test/java/com/albertblanc/AlberBlancTestBase.java
@@ -17,7 +17,7 @@ public class AlberBlancTestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://alberblanc.com/";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
